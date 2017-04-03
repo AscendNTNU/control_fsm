2cb2e921c0742bd27f53d17d59cd547d5ce4830d
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2017-04-03 15:57:26 +0200

    Added some functionality to EstimaeAdjustState, but not yet completed

--- include/control_fsm/EstimateAdjustState.hpp
+++ include/control_fsm/EstimateAdjustState.hpp
@@ -3,11 +3,14 @@
 #include "StateInterface.hpp"
 
 class EstimateAdjustState : public StateInterface {
+private:
+    EventData _cmd;
+
 public:
 	EstimateAdjustState();
 	void handleEvent(ControlFSM& fsm, const EventData& event) override;
 	void stateBegin(ControlFSM& fsm, const EventData& event) override;
-	//void loopState(ControlFSM& fsm); //Uncomment if needed
+	void loopState(ControlFSM& fsm); //Uncomment if needed
 	std::string getStateName() const override { return "EstimatorAdjust"; }
 	const mavros_msgs::PositionTarget* getSetpoint();
 };
--- src/GoToState.cpp
+++ src/GoToState.cpp
@@ -1,22 +1,37 @@
 #include "control_fsm/GoToState.hpp"
 #include "control_fsm/setpoint_msg_defines.h"
 #include <ros/ros.h>
+#include "control_fsm/ControlFSM.hpp"
 
 GoToState::GoToState() {
 	_setpoint.type_mask = default_mask;
 }
 
 void GoToState::handleEvent(ControlFSM& fsm, const EventData& event) {
-	//TODO Implement handle event
+	if(event.eventType == EventType::REQUEST) {
+		if(event.request == RequestType::ABORT || event.request == RequestType::POSHOLD) {
+			fsm.transitionTo(ControlFSM::POSITIONHOLDSTATE, this, event);
+		} else {
+			fsm.handleFSMWarn("Illegal transiton request");
+		}
+	} else if(event.eventType == EventType::COMMAND) {
+		if(event.commandType != CommandType::NONE) {
+			_cmd = event;
+			//TODO Initiate route replan
+		} else {
+			fsm.handleFSMWarn("Recieved command with commandtype NONE - bug!");
+		}
+	}
 }
 
 void GoToState::stateBegin(ControlFSM& fsm, const EventData& event) {
-	_lastEvent = event; //Copy event data for later decisions
+	_cmd = event; //Copy event data for later decisions
 	//TODO Implement rest of stateBegin
 }
 
 void GoToState::loopState(ControlFSM& fsm) {
 	//TODO Implement GoTo state loop.
+    //Remember to offload any kind of heavy computing to another thread/node
 }
 
 const mavros_msgs::PositionTarget* GoToState::getSetpoint() {
