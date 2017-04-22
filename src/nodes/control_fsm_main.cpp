f5f9bf49b87544b09104522e932de2a85dab365c
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2017-04-22 12:26:43 +0200

    Setpoints now published every loop

--- src/nodes/control_fsm_main.cpp
+++ src/nodes/control_fsm_main.cpp
@@ -8,43 +8,43 @@
 #include <mavros_msgs/State.h>
 
 
-bool first_position_recieved = false;
-bool is_armed = false;
-bool is_offboard = false;
+bool firstPositionRecieved = false;
+bool isArmed = false;
+bool isOffboard = false;
 
-constexpr float SETPOINT_PUB_RATE = 1.0 / 30.0f; //In seconds
+constexpr float SETPOINT_PUB_RATE = 30.0f; //In Hz
 
 
 ControlFSM fsm;
 
-void local_pos_callback(const geometry_msgs::PoseStamped& input);
-void state_changed_callback(const mavros_msgs::State& state);
+void localPosCB(const geometry_msgs::PoseStamped& input);
+void mavrosStateChangedCB(const mavros_msgs::State& state);
 
 int main(int argc, char** argv) {
 
 	ros::init(argc, argv, "control_fsm_main");
-	
-	//Subscribe to neccesary topics
 	ros::NodeHandle n;
-	ros::Subscriber local_pos_sub = n.subscribe("mavros/mocap/pose", 10, local_pos_callback);
+	//Subscribe to neccesary topics
+	ros::Subscriber localPosSub = n.subscribe("mavros/local_position/pose", 1, localPosCB);
+	ros::Subscriber mavrosStateChangedSub = n.subscribe("mavros/state", 1, mavrosStateChangedCB);
 	
 	//Set up neccesary publishers
-	ros::Publisher setpoint_pub = n.advertise<mavros_msgs::PositionTarget>("mavros/setpoint_raw/local", 10);
+	ros::Publisher setpointPub = n.advertise<mavros_msgs::PositionTarget>("mavros/setpoint_raw/local", 1);
 
 	//Spin once to get first messages
 	ros::spinOnce();
 
 	//Wait for all systems to initalize and position to become valid
 	ROS_INFO("Waiting for first position msg!");
-	/* TODO Uncomment
-	while(ros::ok() && !first_position_recieved) {
+	while(ros::ok() && !firstPositionRecieved) {
 		ros::spinOnce();
 	}
-	*/
 	ROS_INFO("First position message recieved!");
 
-	ActionServer actionserver(&fsm);
-	ros::Time setpointLastPub = ros::Time::now();
+	ActionServer cmdServer(&fsm);
+	
+	//Used to maintain a fixed loop rate
+	ros::Rate loopRate(SETPOINT_PUB_RATE);
 
 	while(ros::ok()) {
 		//TODO Take get input from planning or other 
@@ -54,37 +54,41 @@ int main(int argc, char** argv) {
 		fsm.loopCurrentState(); //Run current FSM state loop
 
 		//Publish setpoints at gived rate
-		if(ros::Time::now() - setpointLastPub >= ros::Duration(SETPOINT_PUB_RATE)) {
-			const mavros_msgs::PositionTarget* pSetpoint = fsm.getSetpoint();
-			setpoint_pub.publish(*pSetpoint);
-			setpointLastPub = ros::Time::now();
-		}
+		const mavros_msgs::PositionTarget* pSetpoint = fsm.getSetpoint();
+		setpointPub.publish(*pSetpoint);
+
+		//Sleep for remaining time
+ 		loopRate.sleep();
+
 	}
 
 	return 0;
 }
 
-void local_pos_callback(const geometry_msgs::PoseStamped& input) {
+void localPosCB(const geometry_msgs::PoseStamped& input) {
 	fsm.setPosition(input);
+	if(!firstPositionRecieved) {
+		firstPositionRecieved = true;
+	}
 }
 
-void state_changed_callback(const mavros_msgs::State& state) {
+void mavrosStateChangedCB(const mavros_msgs::State& state) {
 	bool offboardTrue = (state.mode == "OFFBOARD");
 	//Only act if relevant states has changed
-	if(offboardTrue != is_offboard || state.armed != is_armed) {
+	if(offboardTrue != isOffboard || state.armed != isArmed) {
 		//Check if old state was autonomous
 		//=> now in manual mode
-		if(is_offboard && is_armed) {
+		if(isOffboard && isArmed) {
 			EventData manualEvent;
 			manualEvent.eventType = EventType::MANUAL;
 			fsm.handleEvent(manualEvent);
 		}
 		//Set current state
-		is_offboard = offboardTrue;
-		is_armed = state.armed;
+		isOffboard = offboardTrue;
+		isArmed = state.armed;
 
 		//If it is armed and in offboard - notify AUTONOMOUS mode
-		if(is_armed && is_offboard) {
+		if(isArmed && isOffboard) {
 			EventData autonomousEvent;
 			autonomousEvent.eventType = EventType::AUTONOMOUS;
 			fsm.handleEvent(autonomousEvent);
