8523c37cba042e633cd04c0b184ac3f00618d7cc
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2017-04-23 13:56:45 +0200

    Added callbacks to FSM for when the state changes and for the different error/warn/info outputs.  Also publishing err/warn/info to topics

--- src/nodes/control_fsm_main.cpp
+++ src/nodes/control_fsm_main.cpp
@@ -6,79 +6,137 @@
 #include <mavros_msgs/PositionTarget.h>
 #include <mavros_msgs/State.h>
 #include <ascend_msgs/ControlFSMEvent.h>
+#include <std_msgs/String.h>
 
 
-bool first_position_recieved = false;
-bool is_armed = false;
-bool is_offboard = false;
+bool firstPositionRecieved = false;
+bool isArmed = false;
+bool isOffboard = false;
 
-constexpr double SETPOINT_PUB_RATE = 30.0f; //In Hz
+constexpr float SETPOINT_PUB_RATE = 30.0f; //In Hz
 
+std::string fsmOnStateChangedPubTopic = "control/fsm/state_changed";
+std::string fsmErrorPubTopic = "control/fsm/on_error";
+std::string fsmWarnPubTopic = "control/fsm/on_warn";
+std::string fsmInfoPubTopic = "control/fsm/on_info";
+int statusMsgBufferSize = 10;
 
 ControlFSM fsm;
 
-void local_pos_callback(const geometry_msgs::PoseStamped& input);
-void state_changed_callback(const mavros_msgs::State& state);
+void localPosCB(const geometry_msgs::PoseStamped& input);
+void mavrosStateChangedCB(const mavros_msgs::State& state);
 
 EventData generateDebugEvent(ascend_msgs::ControlFSMEvent::Request&);
 bool handleDebugEvent(ascend_msgs::ControlFSMEvent::Request&, ascend_msgs::ControlFSMEvent::Response&);
+
+void loadParams(ros::NodeHandle& n);
 
 
 int main(int argc, char** argv) {
 
 	ros::init(argc, argv, "control_fsm_main");
-	
-	//Subscribe to neccesary topics
 	ros::NodeHandle n;
-	ros::Subscriber local_pos_sub = n.subscribe("mavros/mocap/pose", 10, local_pos_callback);
-	
-	ros::Publisher setpoint_pub = n.advertise<mavros_msgs::PositionTarget>("mavros/setpoint_raw/local", 10);
+	ros::NodeHandle np("~");
 
+	//Load ros params
+	loadParams(np);
+
+	//Subscribe to neccesary topics
+	ros::Subscriber localPosSub = n.subscribe("mavros/local_position/pose", 1, localPosCB);
+	ros::Subscriber mavrosStateChangedSub = n.subscribe("mavros/state", 1, mavrosStateChangedCB);
+	
+	//Set up neccesary publishers
+	ros::Publisher setpointPub = n.advertise<mavros_msgs::PositionTarget>("mavros/setpoint_raw/local", 1);
+	ros::Publisher fsmOnStateChangedPub = n.advertise<std_msgs::String>(fsmOnStateChangedPubTopic, statusMsgBufferSize);
+	ros::Publisher fsmOnErrorPub = n.advertise<std_msgs::String>(fsmErrorPubTopic, statusMsgBufferSize);
+	ros::Publisher fsmOnInfoPub = n.advertise<std_msgs::String>(fsmInfoPubTopic, statusMsgBufferSize);
+	ros::Publisher fsmOnWarnPub = n.advertise<std_msgs::String>(fsmWarnPubTopic, statusMsgBufferSize);
+
+	//Set up services
 	ros::ServiceServer debugServiceServer = n.advertiseService("control_fsm_debug", handleDebugEvent);
 
+	//Spin once to get first messages
 	ros::spinOnce();
 
-	ros::Rate loop_rate(30);
+	//Set FSM callbacks
+	fsm.setOnStateChangedCB([&](){
+		std_msgs::String msg;
+		msg.data = fsm.getState()->getStateName();
+		fsmOnStateChangedPub.publish(msg);
+	});
 
-	//Wait for all systems to initalize
-	while(ros::ok() && !first_position_recieved) {
+	fsm.setOnFSMErrorCB([&](const std::string& errMsg) {
+		std_msgs::String msg;
+		msg.data = errMsg;
+		fsmOnErrorPub.publish(msg);
+	});
+
+	fsm.setOnFSMWarnCB([&](const std::string& warnMsg) {
+		std_msgs::String msg;
+		msg.data = warnMsg;
+		fsmOnWarnPub.publish(msg);
+	});
+
+	fsm.setOnFSMInfoCB([&](const std::string& infoMsg) {
+		std_msgs::String msg;
+		msg.data = infoMsg;
+		fsmOnInfoPub.publish(msg);
+	});
+
+	//Wait for all systems to initalize and position to become valid
+	ROS_INFO("Waiting for first position msg!");
+	while(ros::ok() && !firstPositionRecieved) {
 		ros::spinOnce();
 	}
+	ROS_INFO("First position message recieved!");
 
-	ros::Time setpointLastPub = ros::Time::now();
+	
+	
+	//Used to maintain a fixed loop rate
+	ros::Rate loopRate(SETPOINT_PUB_RATE);
+
 	while(ros::ok()) {
 		//TODO Take get input from planning or other 
 		//TODO Implement actionlib
 
-		ros::spinOnce(); //Handle all incoming messages
+		ros::spinOnce(); //Handle all incoming messages - generates fsm events
 		fsm.loopCurrentState(); //Run current FSM state loop
-		if(ros::Time::now() - setpointLastPub >= ros::Duration(1.0 / SETPOINT_PUB_RATE)) {
-			const mavros_msgs::PositionTarget* pSetpoint = fsm.getSetpoint();
-			setpoint_pub.publish(*pSetpoint);
-			setpointLastPub = ros::Time::now();
-		}
+
+		//Publish setpoints at gived rate
+		const mavros_msgs::PositionTarget* pSetpoint = fsm.getSetpoint();
+		setpointPub.publish(*pSetpoint);
+
+		//Sleep for remaining time
+ 		loopRate.sleep();
 	}
 
 	return 0;
 }
 
-void local_pos_callback(const geometry_msgs::PoseStamped& input) {
+void localPosCB(const geometry_msgs::PoseStamped& input) {
 	fsm.setPosition(input);
+	if(!firstPositionRecieved) {
+		firstPositionRecieved = true;
+	}
 }
 
-void state_changed_callback(const mavros_msgs::State& state) {
+void mavrosStateChangedCB(const mavros_msgs::State& state) {
 	bool offboardTrue = (state.mode == "OFFBOARD");
 	//Only act if relevant states has changed
-	if(offboardTrue != is_offboard || state.armed != is_armed) {
-		if(is_offboard && is_armed) {
+	if(offboardTrue != isOffboard || state.armed != isArmed) {
+		//Check if old state was autonomous
+		//=> now in manual mode
+		if(isOffboard && isArmed) {
 			EventData manualEvent;
 			manualEvent.eventType = EventType::MANUAL;
 			fsm.handleEvent(manualEvent);
 		}
+		//Set current state
+		isOffboard = offboardTrue;
+		isArmed = state.armed;
 
-		is_offboard = offboardTrue;
-		is_armed = state.armed;
-		if(is_armed && is_offboard) {
+		//If it is armed and in offboard - notify AUTONOMOUS mode
+		if(isArmed && isOffboard) {
 			EventData autonomousEvent;
 			autonomousEvent.eventType = EventType::AUTONOMOUS;
 			fsm.handleEvent(autonomousEvent);
@@ -171,4 +229,30 @@ EventData generateDebugEvent(ascend_msgs::ControlFSMEvent::Request&req) {
 		})();
 	}
 	return event;
+}
+
+void loadParams(ros::NodeHandle& n) {
+	if(!n.getParam("fsm_error_topic", fsmErrorPubTopic)) {
+		ROS_WARN("Param fsm_error_topic not found");
+	}
+	ROS_INFO("FSM error topic: %s", fsmErrorPubTopic.c_str());
+
+	if(!n.getParam("fsm_warn_topic", fsmWarnPubTopic)) {
+		ROS_WARN("Param fsm_warn_topic not found");
+	}
+	ROS_INFO("FSM warn topic: %s", fsmWarnPubTopic.c_str());
+
+	if(!n.getParam("fsm_info_topic", fsmInfoPubTopic)) {
+		ROS_WARN("Param fsm_info_topic not found");
+	}
+	ROS_INFO("FSM info topic: %s", fsmInfoPubTopic.c_str());
+
+	if(!n.getParam("fsm_state_changed_topic", fsmOnStateChangedPubTopic)) {
+		ROS_WARN("Param fsm_state_changed_topic not found");
+	}
+	ROS_INFO("FSM state changed topic: %s", fsmOnStateChangedPubTopic.c_str());
+	if(!n.getParam("status_msg_buffer_size", statusMsgBufferSize)) {
+		ROS_WARN("Param status_msg_buffer_size not found");
+	}
+	ROS_INFO("Status msg buffer size: %i", statusMsgBufferSize);
 }
