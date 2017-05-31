91a66d2f25cc8f0b0738fbf2761f8c4ef9ab4396
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2017-05-31 13:39:42 +0200

    Ups, forgot to add two neccesary files

---
+++ include/control_fsm/DebugServer.hpp
@@ -0,0 +1,27 @@
+#ifndef DEBUG_SERVER_HPP
+#define DEBUG_SERVER_HPP
+
+#include "ControlFSM.hpp"
+#include <ascend_msgs/ControlFSMEvent.h>
+
+
+
+class DebugServer {
+	using Response = ascend_msgs::ControlFSMEvent::Response;
+	using Request = ascend_msgs::ControlFSMEvent::Request;
+private:
+	ControlFSM* pFsm_ = nullptr;
+	ros::ServiceServer server_;
+	ros::NodeHandle nh_;
+	EventData generateDebugEvent(Request& req);
+	bool handleDebugEvent(Request& req, Response& resp);
+	//Remove copyconstructor and operator=
+	DebugServer operator=(const DebugServer&) = delete;
+	DebugServer(const DebugServer&) = delete;
+
+public:
+	DebugServer(ControlFSM* pFsm);
+
+};
+
+#endif
