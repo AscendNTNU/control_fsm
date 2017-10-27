d7f868253454ce60eda77abbe90f13727c9678b9
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2017-10-27 15:52:52 +0200

    Remove unused header file

--- src/target_tools.cpp
+++ src/target_tools.cpp
@@ -1,10 +1,11 @@
 #include "control_fsm/tools/target_tools.hpp"
 #include <cmath>
-#include <iostream>
 
+///Calculate constants at compiletime
 constexpr double PI = 3.14159265359;
 constexpr double PI_HALF = PI / 2.0;
 constexpr double MAVROS_CORRECTION_PI_HALF = PI_HALF;
+
 double control::getMavrosCorrectedTargetYaw(double current) {
     //Get angle in interval [-pi, pi]
     double theta = std::atan2(std::sin(current), std::cos(current));
