c114f3ba96551464e2909e3ed8c89e8183c76067
atussa <atussak@stud.ntnu.no>
2017-11-02 17:49:32 +0100

    Start path planner interface.

--- include/control_fsm/path_planner.hpp
+++ include/control_fsm/path_planner.hpp
@@ -5,18 +5,43 @@
 #include "include/control_fsm/go_to_state.hpp"
 #include <ascend_msgs/PathPlannerPlan.h>
 #include "include/control_fsm/tools/control_pose.hpp"
+#include <list>
+
+
+#define FIELD_LENGTH 20
+
+
+
+class Node{
+private:
+	float32 x;
+	float32 y;
+
+	// heuristic, use Euclidian distance
+	float32 h;
+	float32 g;
+	// Sum of g and h
+	float32 f;
+
+};
 
 
 class PathPlanner{
 private:
-    
-ascend_msgs::PathPlannerPlan current_plan_;
+	Node graph[FIELD_LENGTH][FIELD_LENGTH];
+
+	std::list<Node> open_list;
+	std::list<Node> closed_list;
+
+	std::list<Node> plan;
+
+	Node end_node;
+	Node start_node;
+	
+	ascend_msgs::PathPlannerPlan current_plan_;
 
 public:
-    PathPlanner();
-    
-    
-    
+    PathPlanner(float32 current_x, float32 current_y, float32 target_x, float32 target_y);
 };
 
 #endif // PATH_PLANNER_HPP
