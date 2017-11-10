6d95807beef62d69fbf41ec2de8d43ddd620aef9
atussa <atussak@stud.ntnu.no>
2017-11-10 20:15:36 +0100

    Alot of changes at once.. Create super simple working A*

--- include/control/planner/path_planner.hpp
+++ include/control/planner/path_planner.hpp
@@ -11,6 +11,8 @@
 #include <iomanip>
 #include <array>
 #include <memory>
+#include <algorithm>
+#include <limits>
 
 
 #define FIELD_LENGTH 20
@@ -21,18 +23,31 @@ private:
     float x;
     float y;
 
-    // heuristic, use Euclidian distance
-    float h;
     float g;
+    // heuristic, use diagonal distance
+    float h;
     // Sum of g and h
     float f;
+
+    float parent_x = -1;
+    float parent_y = -1;
 public:
-    Node();
+    Node(){}
     Node(float x, float y, float g, float h):x(x), y(y), g(g), h(h){f = g+h;}
 
     float getX() const {return x;}
     float getY() const {return y;}
     float getF() const {return f;}
+    float getG() const {return g;}
+    float getParentX() const {return parent_x;}
+    float getParentY() const {return parent_y;}
+
+    void setG(float g) {this->g = g;}
+    void setParentX(float parent_x){this->parent_x = parent_x;}
+    void setParentY(float parent_y){this->parent_y = parent_y;}
+    void updateF(float new_g);
+
+    bool isValid();
 
     // Implemented only for the closed list priority queue
     friend bool operator< (const Node &lhs, const Node &rhs);
@@ -41,22 +56,36 @@ public:
 
 class PathPlanner{
 private:
-    std::array<std::array<std::unique_ptr<Node>, FIELD_LENGTH>, FIELD_LENGTH> graph;
+    std::array<std::array<Node, FIELD_LENGTH>, FIELD_LENGTH> graph;
 
     std::priority_queue<Node> open_list;
-    std::list<Node> closed_list;
+    std::array<std::array<bool, FIELD_LENGTH>, FIELD_LENGTH> closed_list;
 
     std::list<Node> plan;
 
-    std::unique_ptr<Node> end_node;
-    std::unique_ptr<Node> start_node;
+    Node end_node;
+    Node start_node;
+
+    bool destination_reached = false;
 
 public:
     PathPlanner(float current_x, float current_y, float target_x, float target_y);
+
     void initializeGraph();
-    float calculateHeuristic(float x, float y);
-    float calculateG(float x, float y);
+    void initializeClosedList();
+
+    float calculateEuclidianHeuristic(float x, float y);
+    float calculateDiagonalHeuristic(float x, float y);
+
     void printGraph();
+    void relaxGraph();
+
+    void handleSuccessor(float x, float y, float parent_x, float parent_y,
+                         float distance_to_parent);
+    void handleAllSuccessors(float x, float y);
+    bool isDestination(float x, float y);
+
+    void makePlan();
 };
 
 #endif // PATH_PLANNER_HPP
--- src/nodes/path_planner.cpp
+++ src/nodes/path_planner.cpp
@@ -5,28 +5,61 @@ bool operator< (const Node &lhs, const Node &rhs){
 	return lhs.f > rhs.f;
 }
 
+bool Node::isValid(){
+    return (x>0 && x<FIELD_LENGTH && y>0 && y<FIELD_LENGTH);
+}
+
+void Node::updateF(float new_g){
+    if(new_g < g){
+        g = new_g;
+    }
+    f = g+h;
+}
+
+
+
 
 PathPlanner::PathPlanner(float current_x, float current_y, float target_x, float target_y){
-	start_node = std::unique_ptr<Node>(new Node (current_x, current_y, 0, 0));
-    end_node =  std::unique_ptr<Node>(new Node (target_x, target_y, 0, 0));
+    start_node = Node(current_x, current_y, 0, 0);
+    start_node.setParentX(0);
+    start_node.setParentY(0);
+    end_node = Node(target_x,target_y, std::numeric_limits<float>::infinity(), 0);
     initializeGraph();
+    initializeClosedList();
 }
 
 void PathPlanner::initializeGraph(){
     for (int x = 0; x < FIELD_LENGTH; x++){
         for (int y = 0; y < FIELD_LENGTH; y++){
-            graph[x][y] = std::unique_ptr<Node> (new Node(x, y, calculateHeuristic(x,y), calculateG(x,y)));
-            open_list.push(*(graph[x][y]));
+            if(x == start_node.getX() && y == start_node.getY()){
+                graph[x][y] = start_node;
+                open_list.push(graph[x][y]);
+            }
+            /*else if(x == end_node.getX() && y == end_node.getY()){
+                graph[x][y] = end_node;
+            }*/
+            else {
+                graph[x][y] = Node(x,y, std::numeric_limits<float>::infinity(), calculateDiagonalHeuristic(x,y));
+            }
         }
     }
 }
 
-float PathPlanner::calculateHeuristic(float x, float y){
-    return (sqrt((x - end_node->getX())*(x - end_node->getX()) + (y - end_node->getY())*(y - end_node->getY())));
+void PathPlanner::initializeClosedList(){
+    for (int x = 0; x < FIELD_LENGTH; x++) {
+        for (int y = 0; y < FIELD_LENGTH; y++){
+            closed_list[x][y] = false;
+        }
+    }
 }
 
-float PathPlanner::calculateG(float x, float y){
-    return (sqrt((start_node->getX() - x)*(start_node->getX() - x) + (start_node->getY() - y)*(start_node->getY() - y)));
+float PathPlanner::calculateEuclidianHeuristic(float x, float y){
+    return static_cast<float>(sqrt((x - end_node.getX()) * (x - end_node.getX()) +
+                                   (y - end_node.getY()) * (y - end_node.getY())));
+}
+
+float PathPlanner::calculateDiagonalHeuristic(float x, float y) {
+    return (std::max(abs(x-end_node.getX()), abs(y-end_node.getY())));
 }
 
 void PathPlanner::printGraph(){
@@ -34,8 +67,86 @@ void PathPlanner::printGraph(){
         for(int y = 0; y < FIELD_LENGTH; y++){
             std::cout << std::fixed;
             std::cout << std::setprecision(2);
-            std::cout << graph[x][y]->getF() << "  ";
+            std::cout << graph[x][y].getF() << "  ";
         }
         std::cout << std::endl;
+    }
+}
+
+void PathPlanner::relaxGraph(){
+    Node current_node;
+    while(!open_list.empty()){
+        current_node = open_list.top();
+        open_list.pop();
+        float x = current_node.getX();
+        float y = current_node.getY();
+        closed_list[x][y] = true;
+        handleAllSuccessors(x,y);
+        if(destination_reached) {return;}
+    }
+}
+
+void PathPlanner::handleSuccessor(float x, float y, float parent_x, float parent_y, float distance_to_parent) {
+    if(x>= 0 && x < FIELD_LENGTH && y >= 0 && y < FIELD_LENGTH){
+        if(isDestination(x,y)){
+            graph[x][y].setParentX(parent_x);
+            graph[x][y].setParentY(parent_y);
+            destination_reached = true;
+            std::cout << "Destination reached: x=" << x << " y=" << y << std::endl;
+        }
+        else if(closed_list[x][y] == false){
+            float new_g = graph[parent_x][parent_y].getG() + distance_to_parent;
+            if(graph[x][y].getG() > new_g) {
+                std::cout << "New node: " << x << ", " << y << " Parent: "
+                          << parent_x << ", " << parent_y << std::endl;
+                graph[x][y].updateF(new_g);
+                graph[x][y].setParentX(parent_x);
+                graph[x][y].setParentY(parent_y);
+                open_list.push(graph[x][y]);
+            }
+        }
+    }
+}
+
+void PathPlanner::handleAllSuccessors(float x, float y) {
+    // LOVER Å LØSE DETTE PÅ EN BEDRE MÅTE!!!!!!!!!!!!!!!!!!!!
+    handleSuccessor(x+1,y, x, y, 1);
+    if(destination_reached){return;}
+    handleSuccessor(x+1, y+1, x, y, sqrt(2));
+    if(destination_reached){return;}
+    handleSuccessor(x+1, y-1, x, y, sqrt(2));
+    if(destination_reached){return;}
+    handleSuccessor(x, y+1, x, y, 1);
+    if(destination_reached){return;}
+    handleSuccessor(x, y-1, x, y, 1);
+    if(destination_reached){return;}
+    handleSuccessor(x-1, y, x, y, 1);
+    if(destination_reached){return;}
+    handleSuccessor(x-1, y+1, x, y, sqrt(2));
+    if(destination_reached){return;}
+    handleSuccessor(x-1, y-1, x, y, sqrt(2));
+}
+
+bool PathPlanner::isDestination(float x, float y) {
+    return ((x == end_node.getX()) && (y == end_node.getY()));
+}
+
+void PathPlanner::makePlan() {
+    relaxGraph();
+
+    float x = end_node.getX();
+    float y = end_node.getY();
+    
+    while(!(x == start_node.getX() && y == start_node.getY())){
+        plan.push_front(graph[x][y]);
+        float x_temp = graph[x][y].getParentX();
+        float y_temp = graph[x][y].getParentY();
+        x = x_temp;
+        y = y_temp;
+    }
+    plan.push_front(graph[x][y]);
+
+    for (std::list<Node>::iterator it = plan.begin(); it!= plan.end(); ++it){
+        std::cout << "x: " << it->getX() << " y: " << it->getY() << std::endl;
     }
 }
