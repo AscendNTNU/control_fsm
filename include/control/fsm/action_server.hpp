8618008fae9a5c05f20e06c87e88bc4db8680dbf
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2018-01-04 17:30:21 +0100

    Implements an uncoupled action server

--- include/control/fsm/action_server.hpp
+++ include/control/fsm/action_server.hpp
@@ -6,31 +6,45 @@
 #include <ros/ros.h>
 #include <queue>
 
+namespace {
+using boost::shared_ptr;
+using ascend_msgs::ControlFSMGoal_;
+using GoalSharedPtr = shared_ptr<const ControlFSMGoal_<std::allocator<void>>>;
+}
+
+
 class ActionServer {
 private:
+
+    ///Stores what the actionserver wants to do - since last run.
+    std::queue<std::function<void(ControlFSM*)>> event_queue;
+    ///Current goal
+    GoalSharedPtr current_goal;
+    ///ROS nodehandle
     ros::NodeHandle nh_;
     ///Is an action already running?
     bool action_is_running_ = false;
-    ///FSM pointer - tight coupling, sorry
-    ControlFSM* fsm_p_;
     ///Actionserver
     actionlib::SimpleActionServer<ascend_msgs::ControlFSMAction> as_;
     ///Callback for when new action is recieved
     void goalCB();
     ///Callback for when to preempt action
     void preemptCB();
+    ///Start correct action
+    void handleNewGoal(ControlFSM* fsm_p);
     ///Start goto action - send goto cmd to FSM
-    void startGoTo(const ascend_msgs::ControlFSMGoal& goal);
+    void startGoTo(GoalSharedPtr goal_p, ControlFSM* fsm_p);
     ///Start landxy action - send landxy cmd to FSM
-    void startLandXY(const ascend_msgs::ControlFSMGoal& goal);
+    void startLandXY(GoalSharedPtr goal_p, ControlFSM* fsm_p);
     ///Start landgb - NB not in use!!
-    void startLandGB(const ascend_msgs::ControlFSMGoal& goal);
+    void startLandGB(GoalSharedPtr goal_p, ControlFSM* fsm_p);
     ///Start search - go to search alt
-    void startSearch(const ascend_msgs::ControlFSMGoal& goal);
+    void startSearch(GoalSharedPtr goal_p, ControlFSM* fsm_p);
 public:
     ///Constructor
-    ActionServer(ControlFSM* fsm_p);
-    
+    ActionServer();
+    ///Run all generated events - uncoupled from the rest
+    void run(ControlFSM* fsm_p);
 };
 
 #endif
--- src/fsm/action_server.cpp
+++ src/fsm/action_server.cpp
@@ -1,11 +1,10 @@
 #include <control/fsm/action_server.hpp>
-#include <control/fsm/event_data.hpp>
-#include <control/fsm/control_fsm.hpp>
-#include <iostream>
 #include <control/tools/logger.hpp>
 #include <control/tools/config.hpp>
 
-ActionServer::ActionServer(ControlFSM* fsm_p) : fsm_p_(fsm_p), as_(nh_, "controlNodeActionServer", false) {
+constexpr int MAX_ITERATIONS = 100;
+
+ActionServer::ActionServer() : as_(nh_, "controlNodeActionServer", false) {
     if(!ros::isInitialized()) {
         throw control::ROSNotInitializedException();
     }
@@ -15,26 +14,46 @@ ActionServer::ActionServer(ControlFSM* fsm_p) : fsm_p_(fsm_p), as_(nh_, "control
 }
 
 void ActionServer::goalCB() {
+    ///Delay execution
+    event_queue.push([this](ControlFSM *fsm_p) {
+        ActionServer::handleNewGoal(fsm_p);
+    });
+}
+
+void ActionServer::handleNewGoal(ControlFSM *fsm_p) {
+    //Check that there really is a new goal available
+    if(!as_.isNewGoalAvailable()) {
+        return;
+    }
+
+    //Get new goal
+    current_goal = as_.acceptNewGoal();
+    //Check that the client hasn't requested a preempt
+    if(as_.isPreemptRequested()) {
+        as_.setPreempted();
+        return;
+    }
     //Check if an action is already running
     if(action_is_running_) {
         RequestEvent abort_event(RequestType::ABORT);
-        fsm_p_->handleEvent(abort_event);
+        fsm_p->handleEvent(abort_event);
+        if(action_is_running_) {
+            control::handleErrorMsg("Action not terminated, error!");
+        }
     }
-
-    auto goal = *(as_.acceptNewGoal());
     using GOALTYPE = ascend_msgs::ControlFSMActionGoal::_goal_type;
-    switch(goal.cmd) {
+    switch(current_goal->cmd) {
         case GOALTYPE::GO_TO_XYZ:
-            startGoTo(goal);
+            startGoTo(current_goal, fsm_p);
             break;
         case GOALTYPE::LAND_AT_POINT:
-            startLandXY(goal);
+            startLandXY(current_goal, fsm_p);
             break;
         case GOALTYPE::LAND_ON_TOP_OF:
-            startLandGB(goal);
+            startLandGB(current_goal, fsm_p);
             break;
         case GOALTYPE::SEARCH:
-            control::handleWarnMsg("SEARCH not yet implemented");
+            startSearch(current_goal, fsm_p);
             break;
         default:
             control::handleErrorMsg("[Control Action Server] Not a valid or available action");
@@ -42,18 +61,26 @@ void ActionServer::goalCB() {
             break;
     }
 }
+
 //Preempt action and send abort to fsm
 void ActionServer::preemptCB() {
     if(action_is_running_) {
-        RequestEvent abort_event(RequestType::ABORT);
-        fsm_p_->handleEvent(abort_event);
+        event_queue.emplace([this](ControlFSM* fsm_p) {
+            RequestEvent abort_event(RequestType::ABORT);
+            fsm_p->handleEvent(abort_event);
+            if(action_is_running_) {
+                control::handleErrorMsg("Action not terminated, error!");
+            }
+            as_.setPreempted();
+        });
+    } else {
+        as_.setPreempted();
     }
-    as_.setPreempted();
 }
 
 //If goal is goto, send valid goto cmd to fsm
-void ActionServer::startGoTo(const ascend_msgs::ControlFSMGoal& goal) {
-    GoToXYZCMDEvent go_to_event(goal.x, goal.y, goal.y);
+void ActionServer::startGoTo(GoalSharedPtr goal_p, ControlFSM* fsm_p) {
+    GoToXYZCMDEvent go_to_event(goal_p->x, goal_p->y, goal_p->y);
     go_to_event.setOnCompleteCallback([this]() {
         ascend_msgs::ControlFSMResult result;
         result.finished = true;
@@ -74,11 +101,11 @@ void ActionServer::startGoTo(const ascend_msgs::ControlFSMGoal& goal) {
         action_is_running_ = false;
         as_.setAborted(result);
     });
-    fsm_p_->handleEvent(go_to_event);
+    fsm_p->handleEvent(go_to_event);
 }
 //If goal is landxy, send valid landxy cmd to fsm
-void ActionServer::startLandXY(const ascend_msgs::ControlFSMGoal& goal) {
-    LandXYCMDEvent land_xy_event(goal.x, goal.y);
+void ActionServer::startLandXY(GoalSharedPtr goal_p, ControlFSM* fsm_p) {
+    LandXYCMDEvent land_xy_event(goal_p->x, goal_p->y);
     land_xy_event.setOnCompleteCallback([this]() {
         ascend_msgs::ControlFSMResult result;
         result.finished = true;
@@ -99,10 +126,10 @@ void ActionServer::startLandXY(const ascend_msgs::ControlFSMGoal& goal) {
         action_is_running_ = false;
         as_.setAborted(result);
     });
-    fsm_p_->handleEvent(land_xy_event);
+    fsm_p->handleEvent(land_xy_event);
 }
 
-void ActionServer::startLandGB(const ascend_msgs::ControlFSMGoal& goal) {
+void ActionServer::startLandGB(GoalSharedPtr goal_p, ControlFSM* fsm_p) {
     //TODO Implement when landgb procedure is decided
     ROS_WARN("[Control ActionServer] LandGB not implemented!!");
     ascend_msgs::ControlFSMResult result;
@@ -111,8 +138,8 @@ void ActionServer::startLandGB(const ascend_msgs::ControlFSMGoal& goal) {
     as_.setAborted(result);
 }
 
-void ActionServer::startSearch(const ascend_msgs::ControlFSMGoal& goal) {
-    GoToXYZCMDEvent search_event(goal.x, goal.y, control::Config::gb_search_altitude);
+void ActionServer::startSearch(GoalSharedPtr goal_p, ControlFSM* fsm_p) {
+    GoToXYZCMDEvent search_event(goal_p->x, goal_p->y, control::Config::gb_search_altitude);
     search_event.setOnCompleteCallback([this](){
         ascend_msgs::ControlFSMResult result;
         result.finished = true;
@@ -131,7 +158,22 @@ void ActionServer::startSearch(const ascend_msgs::ControlFSMGoal& goal) {
         action_is_running_ = false;
         as_.setAborted(result);
     });
-    fsm_p_->handleEvent(search_event);
+    fsm_p->handleEvent(search_event);
 
 
+}
+
+void ActionServer::run(ControlFSM *fsm_p) {
+    if(event_queue.empty()) return;
+    //Count number of events to run
+    for(int i = 0; !event_queue.empty(); ++i) {
+        if(i > MAX_ITERATIONS) {
+            control::handleErrorMsg("Too many events in event queue");
+            return;
+        }
+        //Run events in queue
+        auto run_event = event_queue.front();
+        run_event(fsm_p);
+        event_queue.pop();
+    }
 }
