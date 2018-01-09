5897557699aaa3d47c28c33410b90a1008cd986e
Rendell Cale <rendellc@stud.ntnu.no>
2018-01-09 12:45:37 +0100

    Implement first try at obstacle avoidance

--- src/tools/obstacle_avoidance.cpp
+++ src/tools/obstacle_avoidance.cpp
@@ -1,14 +1,84 @@
 #include "control/tools/obstacle_avoidance.hpp"
+#include "control/tools/obstacle_state_handler.hpp"
+#include "control/tools/drone_handler.hpp"
+
+#include <ascend_msgs/GRStateArray.h>
+#include <ascend_msgs/GRState.h>
+
+#include <geometry_msgs/Vector3.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/TransformStamped.h>
+
+#include <tf2/LinearMath/Quaternion.h>
+
+#include <memory>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 
 //Static instance
 std::shared_ptr<control::ObstacleAvoidance> control::ObstacleAvoidance::instance_p_;
 
+constexpr float PI{3.14159265359f};
 
 bool control::ObstacleAvoidance::doObstacleAvoidance(mavros_msgs::PositionTarget* setpoint) {
-    //TODO Implement obstacle avoidance
+    //TODO move to class
+    constexpr float clearence_side{0.7f};
+    constexpr float clearence_back{0.5f};
+    constexpr float clearence_front{1.6f};
+    const float clearence_max = std::max({clearence_side, clearence_back, clearence_front}); 
+
+    bool setpoint_modified = false;
+    
+    const std::vector<ascend_msgs::GRState> obstacles = control::ObstacleStateHandler::getCurrentObstacles();
+    const geometry_msgs::PoseStamped drone_pose = control::DroneHandler::getCurrentPose();
+
+    for (const auto& obstacle : obstacles){
+
+        geometry_msgs::Vector3 delta_drone_obstacle;
+        delta_drone_obstacle.x = obstacle.x - drone_pose.pose.position.x;
+        delta_drone_obstacle.y = obstacle.y - drone_pose.pose.position.y; 
+        delta_drone_obstacle.z = drone_pose.pose.position.z; 
+        const float distance_to_obstacle = std::sqrt(std::pow(delta_drone_obstacle.x, 2) + std::pow(delta_drone_obstacle.y, 2));
+        if (distance_to_obstacle < clearence_max){
+            // perform obstacle avoidance
+            const float angle = std::atan2(delta_drone_obstacle.y, delta_drone_obstacle.x) - obstacle.theta;
+            ROS_DEBUG("[control]: angle to obstacle: %.3f", angle);
+            
+            // obstacle.theta + pi/2 is straight ahead for obstacle.
+
+            const bool drone_in_front_of_obstacle = angle >= 0; 
+            
+            geometry_msgs::Vector3 minimum_delta;
+            minimum_delta.z = 0.0; // not used
+            if (drone_in_front_of_obstacle){
+                ROS_DEBUG_THROTTLE(1, "[control]: drone in front of obstacle");
+
+                minimum_delta.x = clearence_side * std::cos(angle);
+                minimum_delta.y = clearence_front/clearence_side * (clearence_side - std::abs(minimum_delta.x));
+            }
+            else {
+                ROS_DEBUG_THROTTLE(1,"[control]: drone behind obstacle");
+
+                minimum_delta.x = clearence_side * std::cos(angle);
+                minimum_delta.y = clearence_back * std::sin(angle);
+            } 
+
+            const float minimum_distance = std::sqrt(std::pow(minimum_delta.x, 2) + std::pow(minimum_delta.y, 2));
+            if (distance_to_obstacle < minimum_distance) {
+                // need to avoid obstacle
+                setpoint_modified = true;
+                setpoint->position.x = obstacle.x + minimum_delta.x;
+                setpoint->position.y = obstacle.y + minimum_delta.y;
+
+                ROS_DEBUG_THROTTLE(1, "[control]: obstacle avoidance setpoint: %.2f, %.2f, %.2f", 
+                        setpoint->position.x, setpoint->position.y, setpoint->position.z);
+            }
+        }
+    }
 
     //Return true if setpoint has been modified.
-    return false;
+    return setpoint_modified;
 }
 
 void control::ObstacleAvoidance::onModified() {
@@ -44,3 +114,4 @@ void control::ObstacleAvoidance::removeOnModifiedCBPtr(const std::shared_ptr<std
         }
     }
 }
+
