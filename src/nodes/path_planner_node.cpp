f15a7301adcb14e946101f280120c5794f624df3
atussa <atussak@stud.ntnu.no>
2018-02-16 16:29:47 +0100

    Make service for goal point

--- src/nodes/path_planner_node.cpp
+++ src/nodes/path_planner_node.cpp
@@ -9,6 +9,7 @@
 #include <mavros_msgs/PositionTarget.h>
 #include "control/tools/planner_config.hpp"
 #include "ascend_msgs/PointArray.h"
+#include "ascend_msgs/PathPlanner.h"
 
 using control::pathplanner::PathPlanner;
 
@@ -28,14 +29,14 @@ struct PlannerState{
 };
 
 
-//Terminates current goal when requested.
-void preemptCB(ActionServerType* server, PlannerState* planner_state) {
-	planner_state->make_plan = false;
-	//Abort whatever you are doing first!
-	ROS_WARN("Preempted!");
-}
 
-void newPlanCB(ActionServerType* server, PlannerState* planner_state){
+using Request = ascend_msgs::PathPlanner::Request;
+using Response = ascend_msgs::PathPlanner::Response;
+
+bool newPlanCB(Request &req, Response &res, PlannerState* planner_state){
+
+	ROS_INFO("newPlanCB");
+
 	planner_state->make_plan = true;
 
 	// Update planner state
@@ -44,20 +45,13 @@ void newPlanCB(ActionServerType* server, PlannerState* planner_state){
 	planner_state->current_x = position.x;
 	planner_state->current_y = position.y;
 
-	if(!server->isNewGoalAvailable()){
-		return;	
-	}
-
 	//Accept the new goal
 	planner_state->new_goal = true;
-	auto goal = server->acceptNewGoal();
-	planner_state->goal_x = goal->x;
-	planner_state->goal_y = goal->y;
 
-	if(server->isPreemptRequested()) {
-		//Goal is already stopped by client
-		return;
-	}
+	planner_state->goal_x = req.goal_x;
+	planner_state->goal_y = req.goal_y;
+
+	return true;
 }
 
 
@@ -77,6 +71,8 @@ void updateSetpointCB(mavros_msgs::PositionTarget::ConstPtr msg_p){
 
 int main(int argc, char** argv){
 
+	ROS_INFO("Path planner node started");
+
 	PlannerState planner_state;
 	PathPlanner plan;
 
@@ -86,49 +82,53 @@ int main(int argc, char** argv){
  
     control::PlannerConfig::loadParams();
 
-    ActionServerType server(n, "plan_path", false);
 
-    server.registerGoalCallback(boost::bind(newPlanCB, &server, &planner_state));
-    server.registerPreemptCallback(boost::bind(preemptCB, &server, &planner_state));
-    server.start();
-
-    ros::Subscriber sub_obstacles = n.subscribe(control::PlannerConfig::obstacle_state_topic, 1, updateObstaclesCB);
+    //ros::Subscriber sub_obstacles = n.subscribe(control::PlannerConfig::obstacle_state_topic, 1, updateObstaclesCB);
     ros::Subscriber sub_setpoint = n.subscribe(control::PlannerConfig::mavros_setpoint_topic, 1, updateSetpointCB);
 
     ros::Publisher pub_plan = n.advertise<ascend_msgs::PointArray>(control::PlannerConfig::plan_points_topic, 1);
+
+    ros::ServiceServer server = n.advertiseService<Request, Response>("path_planner_service", boost::bind(&newPlanCB, _1, _2, &planner_state));
 
 
     while(ros::ok()){
 
     	ros::spinOnce();
 
-    	plan.refreshObstacles(obstacle_coordinates);
+    	//plan.refreshObstacles(obstacle_coordinates);
 
     	// Make new plan as long as a plan is requested and the current one is invalid or the goal is changed
-    	if(planner_state.make_plan && (!plan.isPlanSafe(setpoint_x,setpoint_y) || planner_state.new_goal)){
-    		ROS_INFO("Make new plan.");
+    	if(planner_state.make_plan && (!plan.isPlanSafe(/*setpoint_x,setpoint_y*/19.9,19.9) || planner_state.new_goal)){
+    		//ROS_INFO("Make new plan.");
     		planner_state.new_goal = false;
 		    plan.makePlan(planner_state.current_x, planner_state.current_y, planner_state.goal_x, planner_state.goal_y);
 		    std::list<Node> points = plan.getSimplePlan();
-		    geometry_msgs::Point32 point_32;
+
 		    geometry_msgs::Point point;
 		    // Iterate through the points in the plan and publish to pathplanner-action
 		    ascend_msgs::PathPlannerFeedback feedback;
 		    // For saving memory
 		    feedback.points_in_plan.reserve(20);
-		    ascend_msgs::PointArray plan_to_send;
-		    plan_to_send.points.reserve(20);
-		    for(std::list<Node>::iterator it = points.begin(); it != points.end(); it++){
-        		point_32.x = it->getX();
-        		point_32.y = it->getY();	
+		    ascend_msgs::PointArray points_in_plan;
+		    points_in_plan.points.reserve(20);
+		    
+		    // The first point in the plan is the current point of the drone, so it doesn't need to be sent as part of the plan
+		    std::list<Node>::iterator second_point = points.begin();
+		    second_point++;
+
+		    std::cout << "Published points:\t";
+		    for(std::list<Node>::iterator it = second_point; it != points.end(); it++){
+
         		point.x = it->getX();
         		point.y = it->getY();
         		
-        		feedback.points_in_plan.push_back(point_32);
-        		plan_to_send.points.push_back(point);
+        		points_in_plan.points.push_back(point);
+
+        		std::cout << point.x << ", " << point.y << "\t";
+        	
     		}
-    		server.publishFeedback(feedback);
-    		pub_plan.publish(plan_to_send);
+    		pub_plan.publish(points_in_plan);
+    		std::cout << std::endl;
     	}
     	else{
     		rate.sleep();
--- src/nodes/test_path_planner_node.cpp
+++ src/nodes/test_path_planner_node.cpp
@@ -1,12 +1,44 @@
 #include <ros/ros.h>
-
+#include "ascend_msgs/PathPlanner.h"
 
 
 int main(int argc, char** argv){
 
+	ROS_INFO("Test path planner node started");
+	
 	ros::init(argc, argv, "path_planner_test");
     ros::NodeHandle n;
     ros::Rate rate(30.0);
 
-    ros::Publisher pub_goal = n.advertise<>();
+    ros::ServiceClient client  = n.serviceClient<ascend_msgs::PathPlanner>("path_planner_service");
+
+    ascend_msgs::PathPlanner clientCall;
+
+    clientCall.request.goal_x = 19.9;
+	clientCall.request.goal_y = 19.9;
+
+	if(client.call(clientCall)){
+		ROS_INFO("Request handled");
+	}
+	else{
+		ROS_WARN("Error!");
+	}
+ 	
+ 	/*while(ros::ok()){
+ 		ROS_INFO("Ros is ok");
+ 		ros::spin();
+ 	}
+
+	ros::Duration(3).sleep();
+
+	clientCall.request.goal_x = 1;
+	clientCall.request.goal_y = 8;
+
+	if(client.call(clientCall)){
+		ROS_INFO("Request handled");
+	}
+	else{
+		ROS_WARN("Error!");
+	}*/
+
 }
