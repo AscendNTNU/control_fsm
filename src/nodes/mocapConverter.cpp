837f9d82f61af4a1f502c53bc28bf2d72d4f3c76
drohatha <drohatha@github.com>
2018-03-12 13:44:14 +0100

    Dont f*ck copy paste

--- src/nodes/mocapConverter.cpp
+++ src/nodes/mocapConverter.cpp
@@ -56,7 +56,7 @@ void roombaCallback(const geometry_msgs::PoseStamped& input) {
     ai_msg.ground_robots[0].x = roomba_pose.x; 
     ai_msg.ground_robots[0].y = roomba_pose.y;
 
-    if(fabs(roomba_pose.x - ai_msg.drone_position.x) < 0.5 && fabs(roomba_pose.x - ai_msg.drone_position.x) < 0.5){
+    if(fabs(roomba_pose.x - ai_msg.drone_position.x) < 0.5 && fabs(roomba_pose.y - ai_msg.drone_position.y) < 0.5){
     	ai_msg.ground_robots[0].visible = true; 
     	ai_msg.ground_robots[0].downward_tracked = true; 
     }else{
