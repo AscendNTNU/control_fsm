1e66e2a8a3bc693e79bbc12a813b598ebc4433e4
Rendell Cale <rendellc@stud.ntnu.no>
2018-04-05 20:21:51 +0200

    Fix position hold

--- include/control/fsm/position_hold_state.hpp
+++ include/control/fsm/position_hold_state.hpp
@@ -12,7 +12,6 @@ public:
     PositionHoldState();
     void handleEvent(ControlFSM& fsm, const EventData& event) override;
     void stateBegin(ControlFSM& fsm, const EventData& event) override;
-    void loopState(ControlFSM& fsm) override;
     void stateInit(ControlFSM& fsm) override;
     std::string getStateName() const override { return "Position hold"; }
     ascend_msgs::ControlFSMState getStateMsg() const override;
