53f7fdb2a439193cd12bcce7a922beb65044707c
atussa <atussak@stud.ntnu.no>
2018-05-02 09:30:54 +0200

    If drone is outside field, pretend like it is on the edge of the field

--- include/control/planner/path_planner.hpp
+++ include/control/planner/path_planner.hpp
@@ -114,8 +114,12 @@ public:
     float max_f = 0;
 };
 
-// Convert between coordinate x or y in metres and index in graph
-//int coordToIndex(float k);
+void correctCoordinates(float &x, float &y){
+    if (x < 0) {x = 0.0;}
+    else if (x >= FIELD_LENGTH) {x = FIELD_LENGTH-0.1;}
+    if (y < 0) {y = 0.0;}
+    else if (y >= FIELD_LENGTH) {y = FIELD_LENGTH-0.1;}
+}
 
 }
 }
